d4900e781ae7960d8c5c44b4724115070ea2e069
bhubadinn <phoobadin.wrk.2200@gmail.com>
2022-10-28 14:09:38 +0700

    cleanup

--- index.js
+++ index.js
@@ -1,28 +1,130 @@
-import pipeLineData from "./pipeLineData.json" assert {type: "json"};
+import pipeLineData from "./pipeLineDataCUSA.json" assert {type: "json"};
+import pipeLineData from "./pipeLineDataCUSB.json" assert {type: "json"};
+import pipeLineData from "./pipeLineDataCUSC.json" assert {type: "json"};
+import pipeLineData from "./pipeLineDataCUSD.json" assert {type: "json"};
+import pipeLineData from "./pipeLineDataCUSE.json" assert {type: "json"};
+import pipeLineData from "./pipeLineDataCUSF.json" assert {type: "json"};
+import pipeLineData from "./pipeLineDataCUSG.json" assert {type: "json"};
+import pipeLineData from "./pipeLineDataCUSH.json" assert {type: "json"};
+import pipeLineData from "./pipeLineDataCUSI.json" assert {type: "json"};
+import pipeLineData from "./pipeLineDataCUSJ.json" assert {type: "json"};
+import pipeLineData from "./pipeLineDataCUSK.json" assert {type: "json"};
 
-function checkHowManyDataNode(data) {
-  if (Object.keys(data).length === 0) {
-    return console.log("No data");
+const waterHead = pipeLineData.waterHead;
+const lowerVel = 5;
+const upperVel = 9;
+let remainWaterHead = waterHead;
+let netPipeDiameter = [];
+let optimumVelocity = [];
+
+function calLossOfHead(param) {
+  let resultQ = 0;
+  let resultC = 0;
+  let resultD = 0;
+  let resultDown = 0;
+  let resultFinal = 0;
+  let result = [];
+
+  resultQ = param * 0.0283168466;
+  resultQ = Math.pow(resultQ, 1.852);
+  resultQ = resultQ * 10.67;
+  resultC = Math.pow(1.27, 1.852);
+
+  for (let i = 0, lengthMeter = 0; i < pipeLineData.pipeDiameter.length; i++) {
+    lengthMeter = pipeLineData.pipeDiameter[i] * 0.0254;
+    resultD = Math.pow(lengthMeter, 4.8704);
+    resultDown = resultC * resultD;
+    resultFinal = resultQ / resultDown;
+    result.push(resultFinal * 0.304);
   }
+  return result;
+}
 
-  let node = 1;
-  let nodeName = "";
+function calOptimalResult(param, nodeIndex) {
+  let result = [];
+  let cal = 0;
+  let prevCal = 0;
+  let prevFlowVelocity = 0;
+  let flowVelocity = 0;
 
-  for (let i = 0; i < node; i++) {
-    nodeName = Object.keys(data)[i];
-    console.log(nodeName);
-    if (pipeLineData[`${nodeName}`].length > 1) {
-      node = pipeLineData[`${nodeName}`].length - 1;
-      for (let i = 1; i < pipeLineData[`${nodeName}`].length; i++) {
-        console.log(pipeLineData[`${nodeName}`][i]);
+  for (let i = 0; i < param.length; i++) {
+    cal = (param[i] / 1000) * pipeLineData.nodeInfo[nodeIndex].pipeLength;
+    if (cal < waterHead) {
+      flowVelocity =
+        1.318 *
+        0.75 *
+        Math.pow(pipeLineData.pipeDiameter[i], 0.63) *
+        Math.pow(cal, 0.54);
+      if (i === 0) {
+        prevFlowVelocity = flowVelocity;
+        prevCal = cal;
+      }
+      if (
+        Math.abs(7 - flowVelocity) < Math.abs(7 - prevFlowVelocity) &&
+        flowVelocity >= lowerVel &&
+        flowVelocity <= upperVel
+      ) {
+        remainWaterHead = remainWaterHead + prevCal - cal;
+        result.pop();
+        netPipeDiameter.pop();
+        optimumVelocity.pop();
+        result.push(cal);
+        netPipeDiameter.push(pipeLineData.pipeDiameter[i]);
+        optimumVelocity.push(flowVelocity);
+        prevCal = cal;
       }
     }
   }
-  console.log("-------------------");
+  return result;
 }
 
-checkHowManyDataNode(pipeLineData);
+function main() {
+  let lossOfHead = [];
+  let optimalResult = [];
 
-// console.log(Object.keys(pipeLineData)[0]);
-const nodeName = "a";
-console.log(pipeLineData[`${nodeName}`]);
+  for (let i = 0; i < pipeLineData.nodeInfo.length; i++) {
+    console.log("");
+    console.log(">>> node name: ", pipeLineData.nodeInfo[i].name);
+
+    lossOfHead.push(calLossOfHead(pipeLineData.nodeInfo[i].flowrate));
+    // console.log(lossOfHead[i]);
+
+    optimalResult.push(calOptimalResult(lossOfHead[i], i));
+    // console.log(optimalResult[i]);
+
+    if (remainWaterHead > 1) {
+      console.log(
+        "----- optimum pipe diameter:   ",
+        netPipeDiameter.toString(),
+        "″"
+      );
+      console.log(
+        "----- optimum velocity:       ~",
+        optimumVelocity.toString(),
+        "ft/s"
+      );
+      console.log(
+        "----- remaining water head:   ~",
+        remainWaterHead.toString(),
+        "ft"
+      );
+      console.log("");
+    } else {
+      console.log("!!! Not enough water head pressure at this node :(");
+      console.log("");
+    }
+  }
+}
+
+main();
+
+
+
+
+
+
+
+
+
+
+
--- test.js
+++ test.js
@@ -1,119 +1,28 @@
-// import pipeLineData from "./pipeLineDataCUSA.json" assert {type: "json"};
-// import pipeLineData from "./pipeLineDataCUSB.json" assert {type: "json"};
-// import pipeLineData from "./pipeLineDataCUSC.json" assert {type: "json"};
-// import pipeLineData from "./pipeLineDataCUSD.json" assert {type: "json"};
-// import pipeLineData from "./pipeLineDataCUSE.json" assert {type: "json"};
-// import pipeLineData from "./pipeLineDataCUSF.json" assert {type: "json"};
-// import pipeLineData from "./pipeLineDataCUSG.json" assert {type: "json"};
-// import pipeLineData from "./pipeLineDataCUSH.json" assert {type: "json"};
-// import pipeLineData from "./pipeLineDataCUSI.json" assert {type: "json"};
-// import pipeLineData from "./pipeLineDataCUSJ.json" assert {type: "json"};
-import pipeLineData from "./pipeLineDataCUSK.json" assert {type: "json"};
+import pipeLineData from "./pipeLineData.json" assert {type: "json"};
 
-const waterHead = pipeLineData.waterHead;
-const lowerVel = 5;
-const upperVel = 9;
-let remainWaterHead = waterHead;
-let netPipeDiameter = [];
-let optimumVelocity = [];
-
-function calLossOfHead(param) {
-  let resultQ = 0;
-  let resultC = 0;
-  let resultD = 0;
-  let resultDown = 0;
-  let resultFinal = 0;
-  let result = [];
-
-  resultQ = param * 0.0283168466;
-  resultQ = Math.pow(resultQ, 1.852);
-  resultQ = resultQ * 10.67;
-  resultC = Math.pow(1.27, 1.852);
-
-  for (let i = 0, lengthMeter = 0; i < pipeLineData.pipeDiameter.length; i++) {
-    lengthMeter = pipeLineData.pipeDiameter[i] * 0.0254;
-    resultD = Math.pow(lengthMeter, 4.8704);
-    resultDown = resultC * resultD;
-    resultFinal = resultQ / resultDown;
-    result.push(resultFinal * 0.304);
+function checkHowManyDataNode(data) {
+  if (Object.keys(data).length === 0) {
+    return console.log("No data");
   }
-  return result;
-}
 
-function calOptimalResult(param, nodeIndex) {
-  let result = [];
-  let cal = 0;
-  let prevCal = 0;
-  let prevFlowVelocity = 0;
-  let flowVelocity = 0;
+  let node = 1;
+  let nodeName = "";
 
-  for (let i = 0; i < param.length; i++) {
-    cal = (param[i] / 1000) * pipeLineData.nodeInfo[nodeIndex].pipeLength;
-    if (cal < waterHead) {
-      flowVelocity =
-        1.318 *
-        0.75 *
-        Math.pow(pipeLineData.pipeDiameter[i], 0.63) *
-        Math.pow(cal, 0.54);
-      if (i === 0) {
-        prevFlowVelocity = flowVelocity;
-        prevCal = cal;
-      }
-      if (
-        Math.abs(7 - flowVelocity) < Math.abs(7 - prevFlowVelocity) &&
-        flowVelocity >= lowerVel &&
-        flowVelocity <= upperVel
-      ) {
-        remainWaterHead = remainWaterHead + prevCal - cal;
-        result.pop();
-        netPipeDiameter.pop();
-        optimumVelocity.pop();
-        result.push(cal);
-        netPipeDiameter.push(pipeLineData.pipeDiameter[i]);
-        optimumVelocity.push(flowVelocity);
-        prevCal = cal;
+  for (let i = 0; i < node; i++) {
+    nodeName = Object.keys(data)[i];
+    console.log(nodeName);
+    if (pipeLineData[`${nodeName}`].length > 1) {
+      node = pipeLineData[`${nodeName}`].length - 1;
+      for (let i = 1; i < pipeLineData[`${nodeName}`].length; i++) {
+        console.log(pipeLineData[`${nodeName}`][i]);
       }
     }
   }
-  return result;
+  console.log("-------------------");
 }
 
-function main() {
-  let lossOfHead = [];
-  let optimalResult = [];
+checkHowManyDataNode(pipeLineData);
 
-  for (let i = 0; i < pipeLineData.nodeInfo.length; i++) {
-    console.log("");
-    console.log(">>> node name: ", pipeLineData.nodeInfo[i].name);
-
-    lossOfHead.push(calLossOfHead(pipeLineData.nodeInfo[i].flowrate));
-    // console.log(lossOfHead[i]);
-
-    optimalResult.push(calOptimalResult(lossOfHead[i], i));
-    // console.log(optimalResult[i]);
-
-    if (remainWaterHead > 1) {
-      console.log(
-        "----- optimum pipe diameter:   ",
-        netPipeDiameter.toString(),
-        "″"
-      );
-      console.log(
-        "----- optimum velocity:       ~",
-        optimumVelocity.toString(),
-        "ft/s"
-      );
-      console.log(
-        "----- remaining water head:   ~",
-        remainWaterHead.toString(),
-        "ft"
-      );
-      console.log("");
-    } else {
-      console.log("!!! Not enough water head pressure at this node :(");
-      console.log("");
-    }
-  }
-}
-
-main();
+// console.log(Object.keys(pipeLineData)[0]);
+const nodeName = "a";
+console.log(pipeLineData[`${nodeName}`]);
